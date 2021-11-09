e125b08a7e6e32951105c0e7444ec3db016216bc
Boyko Alexey <elorian@yandex.ru>
2021-11-09 17:08:38 +0300

    add jinja2, turn on year born counter

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+import datetime
 
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+today = datetime.datetime.now().year
+day_of_born = datetime.datetime(year=1920, month=1, day=1, hour=00).year
+delta = today-day_of_born
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+template = env.get_template('template.html')
+rendered_page = template.render(
+    winery_age=delta,
+)
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8001), SimpleHTTPRequestHandler)
 server.serve_forever()
