eaadf07f04d9939844ecb642485950314738b6ca
Boyko Alexey <elorian@yandex.ru>
2021-11-09 15:06:35 +0300

    change prise of Isabella wine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+server = HTTPServer(('0.0.0.0', 8001), SimpleHTTPRequestHandler)
 server.serve_forever()
