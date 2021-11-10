540c389fe3fc84d49450032cd36bd04658976db3
Boyko Alexey <elorian@yandex.ru>
2021-11-10 16:42:29 +0300

    add advantageous offer

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 today = datetime.datetime.now().year
 day_of_born = datetime.datetime(year=1920, month=1, day=1, hour=00).year
 delta = today-day_of_born
-excel_data = pandas.read_excel('wine.xlsx', sheet_name='Лист1').to_dict(orient='record')
+excel_data_sale = pandas.read_excel('wine3.xlsx', sheet_name='Лист1').to_dict(orient='record')
 excel_data_extension = pandas.read_excel('wine2.xlsx', sheet_name='Лист1').to_dict(orient='record')
 wine_by_category = collections.defaultdict(list)
 
-for wine in excel_data_extension:
+for wine in excel_data_sale:
     for element in wine:
         if pandas.isna(wine[element]):
             wine[element] = None
@@ -27,7 +27,7 @@ env = Environment(
 template = env.get_template('template.html')
 rendered_page = template.render(
     winery_age=delta,
-    excel_data=excel_data,
+    excel_data=excel_data_sale,
     wine_by_category=wine_by_category,
 )
 
