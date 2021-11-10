1d8124be21ea80a20f4fdc224a0a1210beb86222
Boyko Alexey <elorian@yandex.ru>
2021-11-10 16:48:09 +0300

    clear code

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ today = datetime.datetime.now().year
 day_of_born = datetime.datetime(year=1920, month=1, day=1, hour=00).year
 delta = today-day_of_born
 excel_data_sale = pandas.read_excel('wine3.xlsx', sheet_name='Лист1').to_dict(orient='record')
-excel_data_extension = pandas.read_excel('wine2.xlsx', sheet_name='Лист1').to_dict(orient='record')
 wine_by_category = collections.defaultdict(list)
 
 for wine in excel_data_sale:
