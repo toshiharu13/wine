fe38dd5e612f37e30e516b130d87da7fc2eeae43
Boyko Alexey <elorian@yandex.ru>
2021-11-10 12:36:23 +0300

    connect excel dataframe to template.html

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 today = datetime.datetime.now().year
 day_of_born = datetime.datetime(year=1920, month=1, day=1, hour=00).year
 delta = today-day_of_born
+excel_data = pandas.read_excel('wine.xlsx', sheet_name='Лист1').to_dict(orient='record')
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
@@ -14,9 +15,10 @@ env = Environment(
 template = env.get_template('template.html')
 rendered_page = template.render(
     winery_age=delta,
+    excel_data=excel_data,
 )
-excel_data = pandas.read_excel('wine.xlsx', sheet_name='Лист1')
-print(excel_data.to_dict(orient='record'))
+
+print(excel_data)
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
 
