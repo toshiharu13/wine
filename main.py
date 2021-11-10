45eb293f6ba5e8277864accf128c800e6bfe3dd5
Boyko Alexey <elorian@yandex.ru>
2021-11-10 16:51:49 +0300

    sort imports + line break in main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import collections
-from http.server import HTTPServer, SimpleHTTPRequestHandler
 import datetime
-import pandas
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 from pprint import pprint
 
+import pandas
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-
 
 today = datetime.datetime.now().year
 day_of_born = datetime.datetime(year=1920, month=1, day=1, hour=00).year
 delta = today-day_of_born
-excel_data_sale = pandas.read_excel('wine3.xlsx', sheet_name='Лист1').to_dict(orient='record')
+excel_data_sale = pandas.read_excel(
+    'wine3.xlsx', sheet_name='Лист1'
+).to_dict(orient='record')
 wine_by_category = collections.defaultdict(list)
 
 for wine in excel_data_sale:
