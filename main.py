f90ae104a7239c663922e316b61228b8e57dd4b1
Boyko Alexey <elorian@yandex.ru>
2021-11-15 12:12:46 +0300

    Add argparse func with deafault arguments(filename and listname). Simplify the cycle with <list_of_wines> variable

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import collections
 import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
@@ -14,18 +15,24 @@ logging.basicConfig(
     filemode='w',
     )
 
+parser = argparse.ArgumentParser(
+    description='Страничка виноделов Новое русское вино'
+)
+parser.add_argument('--file', help='имя файла xlsx', default='wine3.xlsx')
+parser.add_argument('--list_in_file', help='имя листа в xlsx файле', default='Лист1')
+args = parser.parse_args()
+
 this_year = int(datetime.datetime.now().year)
 year_of_born = 1920
 winery_age = this_year - year_of_born
 list_of_wines = pandas.read_excel(
-    'wine3.xlsx', sheet_name='Лист1'
+    args.file, sheet_name=args.list_in_file
 ).to_dict(orient='record')
 wine_by_category = collections.defaultdict(list)
 
 for wine in list_of_wines:
-    for element in wine:
-        if pandas.isna(wine[element]):
-            wine[element] = None
+    if pandas.isna(wine['Сорт']):
+        wine['Сорт'] = None
     wine_by_category[wine['Категория']].append(wine)
 
 env = Environment(
@@ -35,7 +42,6 @@ env = Environment(
 template = env.get_template('template.html')
 rendered_page = template.render(
     winery_age=winery_age,
-    excel_data=list_of_wines,
     wine_by_category=wine_by_category,
 )
 
